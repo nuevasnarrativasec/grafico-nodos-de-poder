65da59a8b3730c2a5c408205097ef6efb0d7d191
nuevasnarrativasec <nuevasnarrativasec@gmail.com>
2026-03-31 17:41:44 -0500

    alto de texto

--- js/disclaimers.js
+++ js/disclaimers.js
@@ -24,7 +24,7 @@ const DISCLAIMERS = {
 
     // Roberto Chiabra
     "CP_40728264": {
-        html:          `<p><strong>El Comercio</strong> contactó a la congresista para recoger sus descargos; sin embargo, hasta la publicación de este especial no se obtuvo respuesta.</p>`,
+        html:          `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>`,
         linkDescargos: '#seccion-descargos'
     },
 
